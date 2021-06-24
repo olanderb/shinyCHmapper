4d9208943494478c7dd4bb7349d8642891bc1719
olanderb <billyolander@gmail.com>
2021-06-24 10:40:07 +0000

    Update acled.R

--- acled/acled.R
+++ acled/acled.R
@@ -5,9 +5,9 @@ library(leaflet)
 
 acled <- read_csv("acled/2021-01-01-2021-06-23-Western_Africa.csv")
 
-glimpse(acled)
-
+#select only fatal events
 acled_fat <- acled %>% filter(fatalities > 0 )
+
 
 leaflet(acled_fat) %>% addTiles() %>% addCircleMarkers( lng = acled_fat$longitude, lat = acled_fat$latitude,
                                               color = "red",   fillOpacity = 0, radius = sqrt(acled_fat$fatalities))
